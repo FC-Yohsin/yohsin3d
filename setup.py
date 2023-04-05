fb563ae0634082df37c0ae8a6c45e682dcd6613c
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2023-04-06 03:10:02 +0500

    🎉  Release v0.1.0

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='yohsin3d',
-    version='0.1-alpha',
+    version='0.1.0',
     description='Program your own RoboCup 3D soccer playing agents in python',
-    author=[('Habib Shahzad', 'habibshehzad55@gmail.com'), ('Abuzar Rasool', 'availabuzar@gmail.com'), ('Faaz Abidi', 'hasanfaaz10@gmail.com')],
+    author='Habib Shahzad, Abuzar Rasool, Faaz Abidi',
+    author_email='habibshehzad55@gmail.com, availabuzar@gmail.com, hasanfaaz10@gmail.com',
     url='https://github.com/FC-Yohsin/yohisn3d',
     packages=find_packages(),
     install_requires=[],
