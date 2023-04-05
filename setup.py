d0fcc80d7577666e1a2c07c04a2d4b23c8e0d3b6
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2023-04-06 04:35:15 +0500

    🐛 Missing Init Files in sub packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yohsin3d',
-    version='0.1.1',
+    version='0.1.3',
     description='Program your own RoboCup 3D soccer playing agents in python',
     author='Habib Shahzad, Abuzar Rasool, Faaz Abidi',
     author_email='habibshehzad55@gmail.com, availabuzar@gmail.com, hasanfaaz10@gmail.com',
