f925ba48e8061bd6ef13b436f40d37cebe10b2b2
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2023-04-06 04:46:48 +0500

    🧹 Fix github URL

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Program your own RoboCup 3D soccer playing agents in python',
     author='Habib Shahzad, Abuzar Rasool, Faaz Abidi',
     author_email='habibshehzad55@gmail.com, availabuzar@gmail.com, hasanfaaz10@gmail.com',
-    url='https://github.com/FC-Yohsin/yohisn3d',
+    url='https://github.com/FC-Yohsin/yohsin3d',
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3.11',
