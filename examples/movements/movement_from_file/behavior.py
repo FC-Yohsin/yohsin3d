71f605dfe999b5045264415161c1ef610e1e0755
Abuzar Rasool <availabuzar@gmail.com>
2023-04-13 02:25:54 +0500

    💎 Fixed codefactor issues
    Co-Authored-By: Faaz <57310134+FaazAbidi@users.noreply.github.com>
    Co-Authored-By: Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>

--- examples/movements/movement_from_file/behavior.py
+++ examples/movements/movement_from_file/behavior.py
@@ -6,7 +6,7 @@ from skills import wave_skill
 class MovementBehavior(BaseBehavior):
     def __init__(self, start_coordinates: None, communicator=None, localizer=None) -> None:
         super().__init__(beam_location=start_coordinates,
-                         communicator=communicator,  localizer=localizer)
+                         communicator=communicator, localizer=localizer)
 
     def act(self):
         wave_skill.perform(self.body_model, self.world_model)
